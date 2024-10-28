d442ff6166ecd188695fd52fba9c4ec7df9c2262
thseodud <sonda13578@gmail.com>
2024-10-28 13:46:33 -0400

    Times table function

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-# Put your function here
+def timesTable(number):
+    output = []
+    for i in range(1, number + 1):
+        
+        table = []
+        for x in range(1, number + 1):
+            table.append(i * x)
+   
+        output.append(table)
+    
+    return output
 
-
-# testing
+# Testing
 nums = int(input())
 print(timesTable(nums))
